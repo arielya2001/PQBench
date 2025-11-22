6f7e794644cb614c3a5de12c4edcb1652a995f16
arielya2001 <ariel.yaacobi@msmail.ariel.ac.il>
2025-11-22 15:00:58 +0200

    test

--- statistics_calculation.py
+++ statistics_calculation.py
@@ -30,7 +30,7 @@ for label, group in df.groupby("label"):
         times = [pkt[2] for pkt in row[packet_cols]]
         iats = np.diff(times)
         all_IATs.extend(iats)
-        "test"
+        "test2"
 
         # Client Hello = packet index 3
         CH_sizes.append(row["3"][1])
